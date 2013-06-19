6d6f1775406440842cc16c0e7c705fdf2749b002
bigdreamstudio <541430660@qq.com>
2013-06-19 16:26:01 +0800

    delete all files

--- README.md
+++
@@ -1,5 +0,0 @@
-#My Blog
-
-## License
-
-[Creative Commons](http://creativecommons.org/licenses/by-nc-sa/3.0/)
--- bin/yuicompressor.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-for /f %%i in (dir.txt) do type %%i >> main-min.css
-java -jar yuicompressor-2.4.7.jar --type css --charset utf-8 -o ..\assets\css\main.min.css main-min.css
-del main-min.css
-
-pause
